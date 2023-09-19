01ebd690c4ae255553c23f86879adc40508fed90
Ashwin Venkita Subharaman (s3783614) <s3783614@student.rmit.edu.au>
2023-09-19 15:43:25 +1000

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include "./fifo/fifo.h"
 #include <cstdlib>
+#include <iostream>
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
+    if (argc < 3)
+    {
+        std::cerr << "Usage :: " << argv[0] << " <algorithm> <datafile>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string algorithm = argv[1];
+    std::string datafile = argv[2];
+
+    if (algorithm == "FIFO")
+    {
+        /* code */
+    }
+    else if (algorithm == "SJF")
+    {
+        /* code */
+    }
+    else if (algorithm == "RR")
+    {
+        /* code */
+    }
+    else
+    {
+        std::cerr << "Invalid algorithm / arguments." << std::endl;
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
