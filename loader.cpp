4471a6d80d512b12fdd22a24a3b8bc8583a52eff
Ashwin Venkita Subharaman (s3783614) <s3783614@student.rmit.edu.au>
2023-09-21 11:33:20 +1000

    Added reading datafile into loader
    Added some error handling as well in case reading file dosen't go as planned

--- loader.cpp
+++ loader.cpp
@@ -1,14 +1,15 @@
 #include "loader.h"
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
 
 Loader::Loader(const std::string &file)
 {
     // TODO: Constructor
     if (!loadData(file))
     {
-        std::cerr << "Error loading the data file: " << file << std::endl;
-        // TODO: Error handling
+        throw std::runtime_error("Error loading the data file: " + file);
     }
 }
 
@@ -22,9 +23,27 @@ bool Loader::loadData(const std::string &dataFile)
     std::ifstream file(dataFile);
     if (!file)
     {
-        // Unable to open the file
+        std::cerr << "Error loading the data file: " << dataFile << std::endl;
         return false;
     }
 
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        osp2023::id_type id;
+        osp2023::time_type total_time;
+
+        if (iss >> id >> total_time)
+        {
+            std::cerr << "Error: Invalid data format in file: " << dataFile << std::endl;
+            return false;
+        }
+
+        pcb process(id, total_time);
+        readyQueue.push_back(process);
+    }
+
     file.close();
+    return true;
 }
